e8abfeefc2d848e9405a85ca1bb6c3bce0d861c2
1281659061 <1281659061@qq.com>
2021-06-30 09:30:38 +0800

    处理密码

--- src/com/zhaoyao/com/email/MailTest.java
+++ src/com/zhaoyao/com/email/MailTest.java
@@ -11,16 +11,7 @@ public class MailTest {
 //        mail.setSubject("aaaaaaaaa");  
 //        mail.setMessage("bbbbbbbbbbbbbbbbb");
         
-        
-        mail.setHost("smtp.163.com"); // 设置邮件服务器,如果不用163的,自己找找看相关的  
-        mail.setSender("liuwei9502@163.com");  
-        mail.setReceiver("liuwei9502@163.com"); // 接收人  
-        mail.setUsername("liuwei9502@163.com"); // 登录账号,一般都是和邮箱名一样吧  
-        mail.setPassword("liuwei1281659061"); // 发件人邮箱的登录密码  
-        mail.setSubject("aaaaaaaaa");  
-        mail.setMessage("bbbbbbbbbbbbbbbbb");  
-        
-        
+
         
         new MailUtil().send(mail);  
 	}
